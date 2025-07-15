62150dc989b161677cd81f137615a29501c8ed39
Harkaranjit Kaur <harhundal2000@gmail.com>
2025-07-15 13:55:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,6 +10,9 @@ from langchain_community.embeddings import OllamaEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.chains import RetrievalQA
 import shutil
+from docx import Document
+from langchain.document_loaders import Docx2txtLoader
+
 
 load_dotenv()
 
@@ -36,15 +39,14 @@ input_text=st.text_input("search the topic you want")
 with st.sidebar:
     st.title("Docs for query:")
     uploaded_file = st.file_uploader("Upload a PDF document", type=["pdf"], key="pdf_upload")
+    uploaded_file = st.file_uploader("Upload a Word document", type=["docx"], key="word_upload")
 
 #open ai llm call
 
 output_parser=StrOutputParser()
 ##chain
 chain=prompt|llm|output_parser
-if input_text:
-    st.write(chain.invoke({'question':input_text}))
-    
+
 ##Document
 if uploaded_file is not None:
     with st.spinner("Reading and indexing your document..."):
@@ -56,13 +58,13 @@ if uploaded_file is not None:
             f.write(uploaded_file.read())
         # Load and split
         loader = PyPDFLoader(file_path)
+        loader =Docx2txtLoader(file_path)
         docs = loader.load()
 
         splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
         chunks = splitter.split_documents(docs)
 
         # Embed and store
-        embeddings = OllamaEmbeddings(model="llama2")
         vectordb = FAISS.from_documents(chunks, embeddings)
 
         # QA Chain
@@ -75,9 +77,8 @@ if uploaded_file is not None:
         st.success("Document processed. Ask your questions below :")
 
         # User input
-        query = st.text_input("Ask a question about the document:")
-        
-        if query:
+       
+        if input_text:
             with st.spinner("Thinking..."):
                 result = qa.run(query)
                 st.markdown(f"**Answer:** {result}")
@@ -86,6 +87,10 @@ if uploaded_file is not None:
                     os.remove(file_path)
             except Exception as e:
                 st.warning(f"cleanup failed:{e}")
+elif uploaded_file is None:
+        if input_text:
+            with st.spinner("Searching..."):
+                st.write(chain.invoke({'question':input_text}))
 
 else: 
     st.info(" Please upload a PDF file to get started.")
