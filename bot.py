3267e910f5f89b4709cb449ab3072a3d97e19a24
Harkaranjit Kaur <harhundal2000@gmail.com>
2025-07-19 16:09:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,8 @@
+# ANSWER QUERY RELATED TO TXT,DOCS,PDF FILES SINGLY
+
+
+
+
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser 
 from langchain_community.llms import Ollama
